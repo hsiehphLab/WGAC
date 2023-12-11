ccee8740ab4e4cf91862a09fc9ba72c9009747a1
David Gordon <gordo893@msi.umn.edu>
2023-12-11 15:54:34 -0600

    run_global_align_110k.snake.sh Slurm instead of SGE

--- run_global_align_110k.snake.sh
+++ run_global_align_110k.snake.sh
@@ -1,8 +1,7 @@
-module purge
-module load modules modules-init modules-gs/prod modules-eichler/prod
-
-module load miniconda/4.5.12
+#!/usr/bin/bash -l
 
 
+conda activate snakemake
 
-snakemake -s global_align_110k.snake  --jobname "{rulename}.{jobid}" --drmaa " -w n -V -cwd -e ./log -o ./log {params.sge_opts}  -S /bin/bash " -w 100 --jobs 100 -p -k
+
+snakemake -s global_align_110k.snake  --jobname "{rulename}.{jobid}" --profile profile -w 100 --jobs 100 -p -k
