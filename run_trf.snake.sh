cc400373eab5e6cf45ac35eb084a15c7bd684508
David Gordon <gordo893@msi.umn.edu>
2023-12-11 16:46:09 -0600

    run_trf.snake.sh diff modules and SGE to Slurm

--- run_trf.snake.sh
+++ run_trf.snake.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/bash -l
+
 module purge
 
-module load modules modules-init modules-gs/prod modules-eichler/prod
+conda activate snakemake
 
-module load miniconda/4.5.12
-snakemake -s trf.snake populate_trf_with_links
+
+snakemake -s trf.snake populate_trf_with_links -j 100 
 
 # added Feb 9, 2022 (DG):
 # restart-times was necessary since occasionally got this error:
 # Error recording metadata for finished job ([Errno 2] No such file or directory: 'fugu_trf/chr2_274.fugu'). Please ensure write permissions for the directory /net/eichler/vol27/projects/hprc/nobackups/chm13v2_wgac3/.snakemake
 
-snakemake -s trf.snake --jobname "{rulename}.{jobid}"  --drmaa " -w n -V -cwd -w n -e ./log -o ./log {params.sge_opts} -S /bin/bash" -j 100 -k --rerun --restart-times 1
+snakemake -s trf.snake --jobname "{rulename}.{jobid}" --profile profile  -j 500 -k --rerun-incomplete --restart-times 1
