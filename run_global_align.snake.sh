391da87b79f017352d5f474d16e717298ae75d7b
David Gordon <gordo893@msi.umn.edu>
2023-12-11 16:03:03 -0600

    run_global_align.snake.sh diff modules and Slurm instead of SGE

--- run_global_align.snake.sh
+++ run_global_align.snake.sh
@@ -1,10 +1,6 @@
-module purge
-#module load modules modules-init modules-gs/prod modules-eichler/prod libevent/2.1.3-alpha
-# removing libevent/2.1.3-alpha for centos7
-module load modules modules-init modules-gs/prod modules-eichler/prod
-
-module load miniconda/4.5.12
+#!/usr/bin/bash -l
 
 
+conda activate snakemake
 
-snakemake -s global_align.snake  --jobname "{rulename}.{jobid}" --drmaa " -w n -V -cwd -e ./log -o ./log {params.sge_opts}  -S /bin/bash " -w 100 --jobs 100 -p -k
+snakemake -s global_align.snake  --jobname "{rulename}.{jobid}" --profile profile -w 100 --jobs 100 -p -k
