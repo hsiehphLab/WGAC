5db4273b09ceb76715be65ae2ab985fbaa3efc11
David Gordon <gordo893@msi.umn.edu>
2024-02-08 16:48:50 -0600

    longer_to_shorter_lines.sh using conda activate biopython instead of pip install biopython

--- longer_to_shorter_lines.sh
+++ longer_to_shorter_lines.sh
@@ -11,7 +11,7 @@ do
     szBasename=$(basename ${szFile})
     szOutputFile=${TMPDIR}/wgac_shorter_lines/${szBasename}
     echo ${szOutputFile} >>${szShorter}
-    echo "about to execute: module load python3/3.9.3_anaconda2021.11_mamba && ./longer_to_shorter_lines.py --szInputFile ${szFile} --szOutputFile ${szOutputFile}"
-     module load python3/3.9.3_anaconda2021.11_mamba && ./longer_to_shorter_lines.py --szInputFile ${szFile} --szOutputFile ${szOutputFile}
+    echo "about to execute: source ~/.bashrc && conda activate biopython && ./longer_to_shorter_lines.py --szInputFile ${szFile} --szOutputFile ${szOutputFile}"
+     source ~/.bashrc && conda activate biopython && ./longer_to_shorter_lines.py --szInputFile ${szFile} --szOutputFile ${szOutputFile}
 done <fastawhole.fofn
 
