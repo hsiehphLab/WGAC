b1718d821f6573b6e957eb84839bdbc8ee838adb
David Gordon <gordo893@msi.umn.edu>
2024-02-13 16:39:39 -0600

    run_trim_ends.snake.sh source ~/.bashrc for Athef

--- run_trim_ends.snake.sh
+++ run_trim_ends.snake.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash -l
 
+
+# needed for Athef because it didn't automatically source ~/.bashrc so couldn't conda activate anything
+source ~/.bashrc
+
+
+
 conda activate snakemake
 
 snakemake -s trim_ends.snake  --jobname "{rulename}.{jobid}" --profile profile -w 100 --jobs 200 -p -k
