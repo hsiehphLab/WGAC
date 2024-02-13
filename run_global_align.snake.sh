9ecb7a16a7603f36aa3f5ea464b0434b654239d8
David Gordon <gordo893@msi.umn.edu>
2024-02-13 16:35:38 -0600

    run_global_align.snake.sh so add source ~/.bashrc

--- run_global_align.snake.sh
+++ run_global_align.snake.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash -l
 
 
+# needed for Athef because it didn't automatically source ~/.bashrc so couldn't conda activate anything
+source ~/.bashrc
+
+
+
+
 conda activate snakemake
 
 snakemake -s global_align.snake  --jobname "{rulename}.{jobid}" --profile profile -w 100 --jobs 100 -p -k
