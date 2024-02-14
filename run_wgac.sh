dd5b7e5d7af3d467853f5e000c23851fc9e8431e
David Gordon <gordo893@msi.umn.edu>
2024-02-13 18:18:12 -0600

    run_wgac.sh do not overwrite makefile_wgac.out

--- run_wgac.sh
+++ run_wgac.sh
@@ -5,4 +5,9 @@
 #SBATCH --mem=10g                                                                                  
 #SBATCH --cpus-per-task=1
 
+if [ -f makefile_wgac.out ]
+then
+    mv makefile_wgac.out makefile_wgac.out$(date -d "today" +"%Y.%m%d.%H%M%S")
+fi
+
 make -f makefile_wgac -j 200 >makefile_wgac.out 2>&1
