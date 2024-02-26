76220b9888a257a9ef70a1406c7bda8098d18be2
David Gordon <gordo893@msi.umn.edu>
2024-02-26 13:25:15 -0600

    clean_rsync.sh sbatch_clean.sh Athef's scripts to clean up and copy to cluster

---
+++ clean_rsync.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash -l
+#SBATCH --time=02:00:00
+#SBATCH --mem=10g                                                                                  
+#SBATCH --cpus-per-task=1
+
+make -f makefile_wgac cleanedUp_done
+
+origin_dir="/scratch.global/javid017/HPRCasm"
+relative_path=$(pwd | sed "s|$origin_dir/||")
+destination_dir="/panfs/jay/groups/7/hsiehph/gordo893/samples/multiple_samples/HPRCasm/$relative_path"
+origin_dir="/scratch.global/javid017/HPRCasm/$relative_path"
+
+mkdir -p $destination_dir
+
+rsync -avr . $destination_dir
+
