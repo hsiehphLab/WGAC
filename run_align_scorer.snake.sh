7f1adbcabd1b64d7606e7a365a85d615ec9e2c21
David Gordon <gordo893@msi.umn.edu>
2024-06-12 16:08:46 -0500

    align_scorer.snake align_scorer3.pl run_align_scorer.snake.sh for parallelizing align_scorer2.pl

---
+++ run_align_scorer.snake.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash -l
+
+
+# prepare for using conda
+source /home/hsiehph/shared/bin/initialize_conda.sh
+
+conda activate snakemake
+
+snakemake -s align_scorer.snake  --jobname "{rulename}.{jobid}" --profile profile -w 100 --jobs 300 -p -k
