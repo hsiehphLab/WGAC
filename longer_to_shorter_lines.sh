31855713fbfd66717e7acaf97ff0cbe463796320
David Gordon <gordo893@msi.umn.edu>
2024-08-30 13:47:54 -0500

    longer_to_shorter_lines.sh to fix biopython not found

--- longer_to_shorter_lines.sh
+++ longer_to_shorter_lines.sh
@@ -12,7 +12,14 @@ do
     szBasenameNoGz=`echo $szBasename | sed 's/\.gz//'`
     szOutputFile=${TMPDIR}/wgac_shorter_lines/${szBasenameNoGz}
     echo ${szOutputFile} >>${szShorter}
-    echo "about to execute: source initialize_conda.sh && conda activate biopython && ./longer_to_shorter_lines.py --szInputFile ${szFile} --szOutputFile ${szOutputFile}"
-     source initialize_conda.sh && conda activate biopython && ./longer_to_shorter_lines.py --szInputFile ${szFile} --szOutputFile ${szOutputFile}
+    #echo "about to check which python is being using before conda:"
+    #which python
+    # fixed a problem here in which biopython was not found due to 
+    # an anaconda module being loaded by autosubmitter and for some reason
+    # it overrode the conda activate biopython
+    echo "about to check which python is being using:"
+    module purge && source initialize_conda.sh && conda activate biopython && which python
+    echo "about to execute: module purge && source initialize_conda.sh && conda activate biopython && ./longer_to_shorter_lines.py --szInputFile ${szFile} --szOutputFile ${szOutputFile}"
+    module purge && source initialize_conda.sh && conda activate biopython && ./longer_to_shorter_lines.py --szInputFile ${szFile} --szOutputFile ${szOutputFile}
 done <fastawhole.fofn
 
