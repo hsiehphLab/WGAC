63b8e407c1d274af20ad3a92361bc62df5bd0cd8
David Gordon <gordo893@acn84.agate.msi.umn.edu>
2024-10-04 16:57:24 -0500

    run_wgac.sh decrease time allocated

--- run_wgac.sh
+++ run_wgac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash -l
 
 
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=10g                                                                                  
 #SBATCH --cpus-per-task=1
 
